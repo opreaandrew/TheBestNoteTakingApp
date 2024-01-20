cf14549f427e97596b056c51e338c234171a05c5
opreaandrew <opreaandrew@yahoo.com>
2024-01-20 14:03:17 +0200

    Initial commit

---
+++ src/main/java/ro/andreioprea/TheBestNoteTakingApp/TheBestNoteTakingAppApplication.java
@@ -0,0 +1,13 @@
+package ro.andreioprea.TheBestNoteTakingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBestNoteTakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBestNoteTakingAppApplication.class, args);
+	}
+
+}
